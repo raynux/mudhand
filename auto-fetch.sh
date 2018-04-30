fb8fe8631499828d17e194ff626c07312d90eb46
Ray Sakai <ray.sakai@logical-fabrics.com>
2018-05-01 02:59:34 +0900

    adopting to bitFlyer

--- auto-fetch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while :
-do
-  node fetch.js
-  sleep 5
-done
---
+++ collect-board.js
@@ -0,0 +1,63 @@
+'use strict'
+const _ = require('lodash')
+const axios = require('axios')
+const fs = require('fs-extra')
+const moment = require('moment')
+
+const FETCH_FREQUENCY = 5000
+const HISTRICAL_DIR = './data'
+
+const request = axios.create({
+  baseURL: 'https://api.bitflyer.jp/v1',
+  timeout: 5000,
+  headers: {}
+})
+
+function ladderDiff(base, ladders) {
+  return _(ladders)
+    // .take(max)
+    .transform((r, l) => {
+      r.push({
+        price: l.price,
+        diff: l.price / base,
+        size: l.size
+      })
+      return r
+    }, [])
+    .value()
+}
+
+async function fetchBoard() {
+  const resp = await request.get('/board', {product_code: 'FX_BTC_JPY'})
+  const price = resp.data.mid_price
+
+  return {
+    price,
+    ts: moment().valueOf(),
+    bids: ladderDiff(price, resp.data.bids),
+    asks: ladderDiff(price, resp.data.asks)
+  }
+}
+
+async function main() {
+  const BASE_DIR = `${HISTRICAL_DIR}/${moment().format('YYYYMMDD-HHmmss')}`
+  await fs.mkdir(BASE_DIR)
+
+  let seqNo = 0
+  const timer = setInterval(async () => {
+    try {
+      const data = JSON.stringify(await fetchBoard())
+      const seqStr = `00000${seqNo}`.slice(-6)
+      await fs.writeFile(`${BASE_DIR}/${seqStr}.json`, data)
+
+      console.log(`SEQ NO [ ${seqNo} ] : ${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+      seqNo += 1
+    }
+    catch(e) {
+      console.error(`fetchBoard() failed at SEQ ${seqNo}`)
+      clearInterval(timer)
+      main() // restart
+    }
+  }, FETCH_FREQUENCY)
+}
+main()
