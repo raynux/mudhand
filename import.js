744f0810b7669d9facbc087062392ae400fad79c
Ray Sakai <ray.sakai@logical-fabrics.com>
2018-05-02 14:43:08 +0900

    import from GCS

--- import.js
+++ import.js
@@ -2,41 +2,45 @@
 const _ = require('lodash')
 const fs = require('fs-extra')
 const moment = require('moment')
+const Queue = require('promise-queue')
+const storage = fs.pathExistsSync('./key.json') ?
+  require('@google-cloud/storage')({ keyFilename: './key.json' }) :
+  require('@google-cloud/storage')()
 const {sequelize, Board} = require('./database')
 
-const DATA_DIR = './data'
-
-function getTargets() {
-  const targets = {}
-  for(const dir of fs.readdirSync(DATA_DIR)) {
-    if(!fs.lstatSync(`${DATA_DIR}/${dir}`).isDirectory()) { continue }
-
-    for(const file of fs.readdirSync(`${DATA_DIR}/${dir}`)) {
-      if(!_.get(targets, dir)) { targets[dir] = [] }
-      targets[dir].push(file)
-    }
-  }
-  return targets
-}
+const ROOT_DIR = 'board'
 
 async function main() {
-  const targets = getTargets()
+  const queue = new Queue(1, Infinity)
   await sequelize.sync()
 
-  for(const k of _.keys(targets)) {
-    for(const fname of targets[k]) {
-      const rec = require(`${DATA_DIR}/${k}/${fname}`)
-      await Board.create({
-        timestamp: rec.ts,
-        price: rec.price,
-        seqNo: rec.seqNo,
-        seqStartAt: moment(k, 'YYYYMMDD-HHmmss').toDate(),
-        bids: rec.bids,
-        asks: rec.asks
-      })
-      .catch((e) => console.error(e) )
-      console.log(`${DATA_DIR}/${k}/${fname}`)
-    }
-  }
+  storage.bucket('mudhand')
+    .getFilesStream({
+      prefix: ROOT_DIR
+    })
+    .on('error', console.error)
+    .on('data', async (file) => {
+      if(!file.name.match(/\.json$/)) { return }
+      try {
+        const [buf] = await file.download()
+        const rec = JSON.parse(buf.toString())
+
+        queue.add(() => {
+          console.log(`QUEUE [ ${queue.getQueueLength()}] : ${file.name}`)
+          return Board.create({
+            timestamp: rec.ts,
+            price: rec.price,
+            seqNo: rec.seqNo,
+            seqStartAt: moment(rec.startAt, 'YYYYMMDD-HHmmss').toDate(),
+            bids: rec.bids,
+            asks: rec.asks
+          })
+        })
+      }
+      catch(e) {
+        console.error(e)
+        console.error(file.name)
+      }
+    })
 }
 main()
