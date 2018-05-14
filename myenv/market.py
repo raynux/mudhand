e6bdc161b4d84966b5b9d3ff21e7ef01a9ddfbfe
Ray Sakai <ray.sakai@logical-fabrics.com>
2018-05-14 14:39:44 +0900

    Postion to be a class

--- myenv/market.py
+++ myenv/market.py
@@ -2,6 +2,62 @@ import gym
 import numpy as np
 import gym.spaces as spaces
 
+class Position():
+  POS_BOOL_INDEX = 0
+  POS_PRICE_INDEX = 1
+
+  NO_POSITION = 0
+  LONG_POSITION = 1
+  SHORT_POSITION = 2
+
+  INVALID_REWARD = -100000
+
+  def __init__(self, SEQ_LEN):
+    self.SEQ_LEN = SEQ_LEN
+    self.reset()
+
+  def buy(self, price):
+    if self.has_short_position():   # clear position
+      reward = self.position_price() - price
+      self.set_position(self.NO_POSITION)
+      return reward
+    elif self.has_no_position():    # get a long position
+      self.set_position(self.LONG_POSITION, price)
+      return 0
+
+    return self.INVALID_REWARD  # invalid operation
+
+  def sell(self, price):
+    if self.has_long_position():  # clear position
+      reward = price - self.position_price()
+      self.set_position(self.NO_POSITION)
+      return reward
+    elif self.has_no_position():    # get a long position
+      self.set_position(self.LONG_POSITION, price)
+      return 0
+
+    return self.INVALID_REWARD  # invalid operation
+
+  def reset(self):
+    self.state = np.zeros(self.SEQ_LEN)
+
+  def position_price(self):
+    return self.state[self.POS_PRICE_INDEX]
+
+  def set_position(self, pos_type, price=0):
+    self.state[self.POS_BOOL_INDEX] = pos_type
+    self.state[self.POS_PRICE_INDEX] = price
+
+  def has_no_position(self):
+    return (self.state[self.POS_BOOL_INDEX] == self.NO_POSITION)
+
+  def has_long_position(self):
+    return (self.state[self.POS_BOOL_INDEX] == self.LONG_POSITION)
+
+  def has_short_position(self):
+    return (self.state[self.POS_BOOL_INDEX] == self.SHORT_POSITION)
+
+
 class Market(gym.Env):
   metadata = {'render.modes': ['human']}
 
@@ -37,37 +93,26 @@ class Market(gym.Env):
     super().__init__()
 
     self.HIST_LEN = self.HIST.shape[0]
-    self.pos_state = np.zeros(self.SEQ_LEN)
+    self.position = Position(self.SEQ_LEN)
 
     self.action_space = spaces.Discrete(3) 
-    self.observation_space = spaces.Box(low=0, high=10, shape=(2, self.SEQ_LEN), dtype=np.float)
+    self.observation_space = spaces.Box(low=0, high=10, shape=(2, self.SEQ_LEN), dtype=np.int32)
     self.reset()
 
   def reset(self):
     self.done = False
     self.seq_index = 0
-    self.position = None
+    self.position.reset()
     return self._observe()
 
   def step(self, action):
     current_price = self.HIST[self.seq_index + self.SEQ_LEN]
     reward = 0
 
-    if action == self.STAY:
-      pass
-
-    elif action == self.BUY:
-      if self.position == None:
-        self.position = current_price
-      else:  # violation!
-        reward = -1
-
+    if action == self.BUY:
+      reward = self.position.buy(current_price)
     elif action == self.SELL:
-      if self.position != None:
-        reward = current_price - self.position
-        self.position = None
-      else:  # violation!
-        reward = -1
+      reward = self.position.sell(current_price)
 
     self.seq_index += 1
 
@@ -83,12 +128,4 @@ class Market(gym.Env):
 
   def _observe(self):
     visible_seq = self.HIST[self.seq_index:self.seq_index + self.SEQ_LEN]
-
-    if self.position == None:
-      self.pos_state[0] = 0
-      self.pos_state[1] = 0
-    else:
-      self.pos_state[0] = 1
-      self.pos_state[1] = self.position
-
-    return [visible_seq, self.pos_state]
+    return np.array([visible_seq, self.position.state], dtype=np.int32)
